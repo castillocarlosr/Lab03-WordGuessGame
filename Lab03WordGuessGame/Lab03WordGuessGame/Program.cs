7faa9365ee37fbdb3b17278680f80858fe57a156
castillocarlosr <castillocarlosr@gmail.com>
2019-01-10 11:38:50 -0800

    setting up

---
+++ Lab03WordGuessGame/Lab03WordGuessGame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Lab03WordGuessGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //This bring up the word file to where the program.cs is.
+            string path = "../../../wordFile.txt";
+            CreateFile(path);
+        }
+
+        public static void CreateFile(string pathInside)
+        {
+            try
+            {
+                using(StreamWriter newFileWords = new StreamWriter(pathInside))
+                {
+                    newFileWords.WriteLine("Carlos test is here again");
+                }
+            }
+            catch(IOException eIO)
+            {
+                Console.WriteLine(eIO.Message);
+            }
+            catch (Exception eAll)
+            {
+                Console.WriteLine(eAll.Message);
+                throw;
+            }
+        }
+
+        public static void ReadFile(string pathInisde)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
