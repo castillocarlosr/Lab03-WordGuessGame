5ed5c2fc2b1c1aae446dfc0acdd924a11e7955be
castillocarlosr <castillocarlosr@gmail.com>
2019-01-11 10:20:36 -0800

    working progress2

--- Lab03WordGuessGame/Lab03WordGuessGame/Program.cs
+++ Lab03WordGuessGame/Lab03WordGuessGame/Program.cs
@@ -5,12 +5,17 @@ namespace Lab03WordGuessGame
 {
     public class Program
     {
+        public static string path = "../../../wordFile.txt";
+
         static void Main(string[] args)
         {
-            //Carlos C. 01-04-2019
+            //Carlos C. 01-11-2019
             //This moves up the word file to where the program.cs file is at so it's easy to find.
-            string path = "../../../wordFile.txt";
-            CreateFile(path);
+            //string path = "../../../wordFile.txt";
+            //CreateFile(path);
+            ReadFile(path);
+            Console.WriteLine(path);
+            Console.ReadLine();
         }
 
         /// <summary>
@@ -25,7 +30,7 @@ namespace Lab03WordGuessGame
             {
                 using(StreamWriter newFileWords = new StreamWriter(pathInside))
                 {
-                    newFileWords.WriteLine("Carlos test is here again");
+                    newFileWords.WriteLine("Carlos test is here again 3");
                 }
             }
             catch(IOException eIO)
@@ -46,7 +51,11 @@ namespace Lab03WordGuessGame
                 string[] linesInFile = File.ReadAllLines(pathInisde);
                 for (int i = 0; i < linesInFile.Length; i++)
                 {
-
+                    Console.WriteLine(linesInFile[i]);
+                }
+                for (int j = 0; j < linesInFile.Length; j++)
+                {
+                    Console.WriteLine(linesInFile);
                 }
             }
             catch (Exception)
@@ -54,6 +63,50 @@ namespace Lab03WordGuessGame
 
                 throw;
             }
+        }
+
+        public static string[] GetSingleWord()
+        {
+            string[] allWordsInFile;
+            string answer = "testcarlos";
+
+            allWordsInFile = File.ReadAllLines(path);
+            return allWordsInFile;
+        }
+
+        public void AppendFile(string pathInside)
+        {
+            try
+            {
+                using (StreamWriter newFileWords = File.AppendText(pathInside))
+                {
+                    newFileWords.WriteLine("New line added to Carlos");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        /*
+        public void DeleteFile(string pathInside)
+        {
+            try
+            {
+                File.Delete(pathInside);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        */
+
+        public void SplitsFile()
+        {
+            //Something ....
         }
     }
 }
